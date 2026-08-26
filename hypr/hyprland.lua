6e00aa44574601b6b9c114c294711c96079fcf1f
CrKcel <140730205+CrKcel@users.noreply.github.com>
2026-08-26 12:13:46 +0800

    hypr

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -54,7 +54,7 @@ hl.config({
 
         resize_on_border = false,
         allow_tearing = false,
-        layout = "master",
+        layout = "scrolling",
     },
 
     decoration = {
@@ -178,6 +178,13 @@ hl.bind("XF86MonBrightnessUp", hl.dsp.exec_cmd(ipc .. "brightness-up"))
 hl.bind("XF86MonBrightnessDown", hl.dsp.exec_cmd(ipc .. "brightness-down"))
 
 local mainMod = "SUPER"
+
+for i = 1, 10 do
+    local key = i % 10 -- 10 maps to key 0
+    hl.bind(mainMod .. " + " .. key,             hl.dsp.focus({ workspace = i}))
+    hl.bind(mainMod .. " + SHIFT + " .. key,     hl.dsp.window.move({ workspace = i }))
+end
+
 hl.bind(mainMod .. " + Q", hl.dsp.window.close())
 hl.bind(mainMod .. " + X", hl.dsp.exec_cmd(terminal))
 hl.bind(mainMod .. " + C", hl.dsp.exec_cmd("firefox"))
@@ -185,22 +192,26 @@ hl.bind(mainMod .. " + E", hl.dsp.exec_cmd(fileManager))
 
 hl.bind(mainMod .. " + left",  hl.dsp.focus({ direction = "left" }))
 hl.bind(mainMod .. " + right", hl.dsp.focus({ direction = "right" }))
-hl.bind(mainMod .. " + mouse_up",    hl.dsp.focus({ direction = "left" }))
-hl.bind(mainMod .. " + mouse_down",  hl.dsp.focus({ direction = "right" }))
-
 hl.bind(mainMod .. " + down", hl.dsp.focus({ workspace = "+1" }))
 hl.bind(mainMod .. " + up",   hl.dsp.focus({ workspace = "-1" }))
+
+hl.bind(mainMod .. " + mouse_up",    hl.dsp.focus({ direction = "left" }))
+hl.bind(mainMod .. " + mouse_down",  hl.dsp.focus({ direction = "right" }))
 hl.bind(mainMod .. " + mouse:275",   hl.dsp.focus({ workspace = "+1" }))
 hl.bind(mainMod .. " + mouse:276",   hl.dsp.focus({ workspace = "-1" }))
 
+hl.bind(mainMod .. " + A",  hl.dsp.focus({ direction = "left" }))
+hl.bind(mainMod .. " + D", hl.dsp.focus({ direction = "right" }))
+hl.bind(mainMod .. " + S", hl.dsp.focus({ workspace = "+1" }))
+hl.bind(mainMod .. " + W",   hl.dsp.focus({ workspace = "-1" }))
+
+hl.bind(mainMod .. " + K",  hl.dsp.focus({ direction = "left" }))
+hl.bind(mainMod .. " + J", hl.dsp.focus({ direction = "right" }))
+hl.bind(mainMod .. " + L", hl.dsp.focus({ workspace = "+1" }))
+hl.bind(mainMod .. " + H",   hl.dsp.focus({ workspace = "-1" }))
+
 hl.bind(mainMod .. " + mouse:272", hl.dsp.window.drag(),   { mouse = true })
 hl.bind(mainMod .. " + mouse:273", hl.dsp.window.resize(), { mouse = true })
-
-for i = 1, 10 do
-    local key = i % 10 -- 10 maps to key 0
-    hl.bind(mainMod .. " + " .. key,             hl.dsp.focus({ workspace = i}))
-    hl.bind(mainMod .. " + SHIFT + " .. key,     hl.dsp.window.move({ workspace = i }))
-end
 
 hl.bind(mainMod .. "+Space", hl.dsp.exec_cmd(ipc .. "panel-toggle launcher"))
 hl.bind(mainMod .. "+comma", hl.dsp.exec_cmd(ipc .. "settings-toggle"))
